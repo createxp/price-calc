35e6f46c1c8d61fc2ce078e02ad9be0766aa8017
Trehan Sangpriya <trehansangpriya@gmail.com>
2023-04-25 13:09:00 +0530

    updated Images

--- src/data/faqData/index.ts
+++ src/data/faqData/index.ts
@@ -18,10 +18,10 @@ const faqs = [
         question: 'Can I create hourly rates for multiple profiles?',
         answer: 'Yes! Our tool allows you to create multiple profiles for each service/package you offer.'
     },
-    {
-        question: 'Is my pricing information secure?',
-        answer: 'Yes, we take security very seriously. All pricing information is encrypted and stored securely on our servers.'
-    },
+    // {
+    //     question: 'Is my pricing information secure?',
+    //     answer: 'Yes, we take security very seriously. All pricing information is encrypted and stored securely on our servers.'
+    // },
     {
         question: 'Can I customize the currency?',
         answer: 'Yes, our tool allows you to select your preferred currency from a range of options.'
